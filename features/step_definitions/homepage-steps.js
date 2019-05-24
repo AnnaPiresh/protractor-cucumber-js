e7abe1cf2afa71c1fc34dfc7b6b4b1631f969700
Anna Vasylenko <Anna_Vasylenko@epam.com>
2019-05-24 19:07:45 +0300

    steps updated

--- features/step_definitions/homepage-steps.js
+++ features/step_definitions/homepage-steps.js
@@ -4,32 +4,26 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 
+let searchField = element(by.name('q'));
 
 Given('user opens Google homepage', function () {
     const title = "Google";
-    browser.get('https://www.google.com/');
+    browser.driver.get('https://www.google.com/');
 
     expect(browser.getTitle()).to.eventually.equal(title);
 });
 
 When('user enters search term {string}', function (string) {
-    let searchField = element(by.name('q'));
-
-    browser.waitForAngular(false);
-
     searchField.sendKeys(string);
-    searchField.submit();
 });
 
 When('presses Enter on keyboard', function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+    searchField.submit();
+});
 
 Then('title of page is {string}', function (string) {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-          });
+    expect(browser.getTitle()).to.eventually.equal(string);
+});
 
 
 Then('search results are matching the search term', function () {
