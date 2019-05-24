0cddf338bdf996c8b72d3b6bed2d99fc4b801f11
Anna Vasylenko <Anna_Vasylenko@epam.com>
2019-05-24 18:33:02 +0300

    steps updated

--- features/step_definitions/homepage-steps.js
+++ features/step_definitions/homepage-steps.js
@@ -1,18 +1,25 @@
 const {Given,When,Then} = require('cucumber');
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
 const expect = chai.expect;
 
-Given('user opens Google homepage', function (callback) {
-            browser.get('https://www.google.com/');
 
-            expect(browser.getTitle()).to.equal('Google');
-           return callback();
-         });
+Given('user opens Google homepage', function () {
+    const title = "Google";
+    browser.get('https://www.google.com/');
+
+    expect(browser.getTitle()).to.eventually.equal(title);
+});
 
 When('user enters search term {string}', function (string) {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+    let searchField = element(by.name('q'));
+
+    browser.waitForAngular(false);
+
+    searchField.sendKeys(string);
+    searchField.submit();
+});
 
 When('presses Enter on keyboard', function () {
            // Write code here that turns the phrase above into concrete actions
